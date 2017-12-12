4a4acdf736e245ed5a69db006e70a10514e818cf
Jessicai <jessicabarros@gmail.com>
2017-12-12 19:31:56 -0200

    erro no index resolvido

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
@@ -11,15 +12,19 @@ $app->config('debug', true);
 
 $app->get('/', function() {
 
-    $page = new Page(array(
-        "header"=>false,
-        "footer"=>false
-    ));
+    $page = new Page();
     $page->setTpl("index");
     
 // 	$sql = new Hcode\DB\Sql();
 // 	$results = $sql->select("SELECT * FROM tb_users");
 // 	echo json_encode($results);
+
+});
+
+$app->get('/admin', function() {
+
+    $page = new PageAdmin();
+    $page->setTpl("index");
 
 });
 
